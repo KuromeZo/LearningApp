61033679cdc7c985733ef6a546d66ac7268baab7
KuromeZo <s30068@pjwstk.edu.pl>
2025-06-29 22:33:09 +0200

    1

--- LearningApp/API/DTOs/ExerciseResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LearningApp.API.DTOs;
-
-public class ExerciseResult
-{
-    public bool IsCorrect { get; set; }
-    public string Message { get; set; } = string.Empty;
-}
--- LearningApp/API/DTOs/SubmitSolutionRequest.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LearningApp.API.DTOs;
-
-public class SubmitSolutionRequest
-{
-    public string Code { get; set; } = string.Empty;
-}
